221be0473118321eca410f42c18e956535b2675d
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-08-22 00:03:55 +0200

    add config.ini, add basic logging
    config.ini will contain the secure parameters.
    
    The basic meta data of an incoming request will be logged in /logs that
    is added to .gitignore.

--- update-git.php
+++ update-git.php
@@ -6,27 +6,49 @@
  * @copyright 2020 Benno Bielmeier
  */
 
+define('HTTP_SUCCESS', 200);
+define('HTTP_ERROR', 400);
+
+
 /**
- * Returns whether current request uses SSL.
+ * Return whether current request uses SSL.
  *
  * @return bool
  */
 function isSSL() {
+	# 'HTTPS' set to a non-empty value if the script was queried through the HTTPS protocol.
 	return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-		|| $_SERVER['SERVER_PORT'] == 443;
+	        || $_SERVER['SERVER_PORT'] == 443;
 }
 
 $config = parse_ini_file('config.ini');
+
+/**
+ * Log incoming Request
+ *
+ * @return bool
+ */
+function log_access($path) {
+	if (empty($path))
+		return FALSE;
+
+	$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.$_SERVER['REQUEST_TIME'].PHP_EOL.
+	        "User Agent: ".$_SERVER['HTTP_USER_AGENT'].PHP_EOL.
+	        "-------------------------".PHP_EOL;
+
+	return file_put_contents($path.'/logs/log_'.date("Y-m-d").'.log', $log, FILE_APPEND);
+}
 ?>
 
 <?php
 $DEBUG = TRUE;
+log_access($config['log_dir']);
 if (isSSL() && isset($_POST)) {
 	if ($DEBUG) {
 		echo shell_exec('echo Hello World');
 	}
-	http_response_code(200);
+	http_response_code(HTTP_SUCCESS);
 } else {
-	http_response_code(400);
+	http_response_code(HTTP_ERROR);
 }
 ?>
