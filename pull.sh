3a5212b3751778f9611a11bae226e8742eb92108
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-08-22 03:08:36 +0200

    fix log path in pull.sh

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
-eval "$(ssh-agent -s)" &>> pull.log
-ssh-add deploy_key &>> pull.log
-git pull -v --ff-only 2&>1 | tee pull.log
+eval "$(ssh-agent -s)" &>> .logs/pull.log
+ssh-add deploy_key &>> .logs/pull.log
+git pull -v --ff-only 2&>1 | tee .logs/pull.log
