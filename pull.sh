f7e081aa3782f27a26233cbc30bfe84f9f31108b
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-08-22 03:29:16 +0200

    fix missing path slash

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
-eval "$(ssh-agent -s)" &>> .logs/pull.log
-ssh-add deploy_key &>> .logs/pull.log
-git pull -v --ff-only 2&>1 | tee .logs/pull.log
+eval "$(ssh-agent -s)" &>> ./logs/pull.log
+ssh-add deploy_key &>> ./logs/pull.log
+git pull -v --ff-only 2&>1 | tee ./logs/pull.log
